de4fc029f002c20baf99cfb2ef27fdbbfb43c5ce
hanchi17 <goohanchi17@gmail.com>
2025-04-03 17:00:32 +0900

    노인정 사전순배열

--- Clean.py
+++ Clean.py
@@ -1,16 +1,16 @@
-from DB import no_sub
+from DB import eo_sub
 
-for i in no_sub:
+for i in eo_sub:
     if i == "하스스톤":
         continue
     
-    f = open(f"./노인정/{i}.txt", "r", encoding="UTF8")
+    f = open(f"./어인정/{i}.txt", "r", encoding="UTF8")
     words = [i.strip("\n") for i in f.readlines()]
     f.close()
 
     words = sorted(set(words))
 
-    f = open(f"./노인정/{i}.txt", "w", encoding="UTF8")
+    f = open(f"./어인정/{i}.txt", "w", encoding="UTF8")
     for k in words:
         f.write(k + "\n")
     f.close()
